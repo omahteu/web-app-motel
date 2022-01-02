d982e2bccf9c5d4dc3cd682f167e48830019db3c
Matheus <matheus.mmn@hotmail.com>
2022-01-02 15:20:56 -0300

    Mais Modificações

--- paginas_js/index.js
+++ paginas_js/index.js
@@ -3,9 +3,37 @@ modos = ['a1', 'a2', 'a3', 'a4', 'a5', 'a6']
 $(".reservado").click(function(){
     var quarto = $(this).attr('name')
 
+    // substituir o if por switch
+
     if(quarto === '1'){
         var box = modos.slice(0, 3)
         reservado(quarto, box[0], box[1], box[2])
+    } else (
+        console.log('maior')
+    )
+})
+
+$(".limpeza").click(function(){
+    var quarto = $(this).attr('name')
+
+    // substituir o if por switch
+
+    if(quarto === '1'){
+        var box = modos.slice(0, 3)
+        limpeza(quarto, box[0], box[1], box[2])
+    } else (
+        console.log('maior')
+    )
+})
+
+$(".aguardando").click(function(){
+    var quarto = $(this).attr('name')
+
+    // substituir o if por switch
+
+    if(quarto === '1'){
+        var box = modos.slice(0, 3)
+        aguardando(quarto, box[0], box[1], box[2])
     } else (
         console.log('maior')
     )
@@ -20,6 +48,33 @@ function reservado(q, x, y, z) {
 
     $("[id=botaoq" + q + "]").css('visibility', 'hidden')
 
+    // $(".pes").prop("disabled", true)
+    // $(".cod").prop("disabled", true)
+    // $(".des").prop("disabled", true)
+    // $(".qtd").prop("disabled", true)
+    // $(".vun").prop("disabled", true)
+    // $(".ser").prop("disabled", true)
+    // $(".tot").prop("disabled", true)
+
+    $("#" + x).css('visibility', 'visible')
+    $("#" + x).val('Iniciar')
+
+    $("#" + y).css('visibility', 'visible')
+    $("#" + y).val('Encerrar')
+
+    $("#quarto").text(q)
+    $("#tipo").text('reservado')
+}
+
+function limpeza(q, x, y, z) {
+    $("#quarto" + q).css({
+        "background-color": "#F0E68C",
+        "opacity": 0.5})
+    
+    $("#tempo").css('margin-top', '-30px')
+
+    $("[id=botaoq" + q + "]").css('visibility', 'hidden')
+
     $(".pes").prop("disabled", true)
     $(".cod").prop("disabled", true)
     $(".des").prop("disabled", true)
@@ -27,10 +82,25 @@ function reservado(q, x, y, z) {
     $(".vun").prop("disabled", true)
     $(".ser").prop("disabled", true)
     $(".tot").prop("disabled", true)
-
-    $("#" + x).css('visibility', 'visible')
-    $("#" + x).val('Iniciar')
-
-    $("#" + y).css('visibility', 'visible')
-    $("#" + y).val('Encerrar')
 }
+
+function aguardando(q, x, y, z) {
+    $("#quarto" + q).css({
+        "background-color": "#DAA520",
+        "opacity": 0.5})
+    
+    $("#tempo").css('margin-top', '-30px')
+
+    $("[id=botaoq" + q + "]").css('visibility', 'hidden')
+
+    $(".pes").prop("disabled", true)
+    $(".cod").prop("disabled", true)
+    $(".des").prop("disabled", true)
+    $(".qtd").prop("disabled", true)
+    $(".vun").prop("disabled", true)
+    $(".ser").prop("disabled", true)
+    $(".tot").prop("disabled", true)
+}
+
+// necessário alterar o id, ou buscar pela class
+
