1268ae49a2545a7ad88f9f1b9a1246ad468765d0
Matheus <matheus.mmn@hotmail.com>
2022-01-10 17:15:17 -0300

    implementações de botões

--- paginas_js/box.js
+++ paginas_js/box.js
@@ -5,26 +5,91 @@ modos = ['a1', 'a2', 'a3', 'a4', 'a5', 'a6']
 $("#" + modos[0]).click(function(){
     var quarto = $("#quarto").text()
     var acao = $(this).val()
-    console.log(quarto, acao)
+
+    triagem(acao, quarto, 'a1', 'a2', 'a3')
 })
 
 $("#" + modos[1]).click(function(){
     var quarto = $("#quarto").text()
     var acao = $(this).val()
-    console.log(quarto, acao)
+
+    triagem(acao, quarto, 'a1', 'a2', 'a3')
 })
 
 $("#" + modos[2]).click(function(){
     var quarto = $("#quarto").text()
     var acao = $(this).val()
-    console.log(quarto, acao)
-    desfazer(quarto, 'a1')
+
+    triagem(acao, quarto, 'a1', 'a2', 'a3')
+    
 })
 
 function desfazer(q, x, y, z) {
     $('#quarto' + q). removeAttr('style')
 
-    $("[id=botaoq" + q + "]").css('visibility', 'visible')
+    $("[id=botaoq" + q + "]"). removeAttr('style')
 
+    $("#" + x). removeAttr('style')
+    $("#" + x).val('')
+
+    $("#" + y). removeAttr('style')
+    $("#" + y).val('')
+
+    $("#" + z). removeAttr('style')
+    $("#" + z).val('')
+
+    $("#quarto").text('0')
+    $("#tipo").text('0')
+    $("#entrada").text('')
+    $("#valor-quarto").text('0')
+
+}
+
+function pernoitx(q, x, y, z) {
+    $("#quarto" + q).css({
+        "background-color": "#8B008B",
+        "opacity": 0.5})
+    
+    $("#tempo").css('margin-top', '-30px')
+
+    $("[id=botaoq" + q + "]").css('visibility', 'hidden')
+
+    // $(".pes").prop("disabled", true)
+    // $(".cod").prop("disabled", true)
+    // $(".des").prop("disabled", true)
+    // $(".qtd").prop("disabled", true)
+    // $(".vun").prop("disabled", true)
+    // $(".ser").prop("disabled", true)
+    // $(".tot").prop("disabled", true)
+
+    $("#" + x).css('visibility', 'visible')
+    $("#" + x).val('Alterar P/ Locação')
+
+    $("#" + y).css('visibility', 'visible')
+    $("#" + y).val('Encerrar')
+
+    // $("#" + z).css('visibility', 'visible')
+    // $("#" + z).val('Cancelar Reserva')
+
+    $("#quarto").text(q)
+    $("#tipo").text('pernoite')
+}
+
+function triagem(acao, quarto, x, y, z){
+
+    switch(acao){
+        case "Disponibilizar Quarto":
+            desfazer(quarto, x, y, z)
+            break
+        case "Cancelar Reserva":
+            desfazer(quarto, x, y, z)
+            break
+        case "Encerrar":
+            desfazer(quarto, x, y, z)
+            break
+        case "Alterar P/ Pernoite":
+            pernoitx(quarto, x, y, z)
+            break
+    }
 
 }
